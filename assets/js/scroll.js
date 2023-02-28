b00f7aebae9372cc3de85f01256d1f437c6437f6
Dalala <nik.dalalishvili@gmail.com>
2023-02-28 15:47:11 +0100

    ref V~1.2.4

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,8 +2,9 @@ window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`); // Update method
 });
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-let smoother = ScrollSmoother.create({
-	wrapper: ".wrapper",
-	content: ".content"
-});
+
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// let smoother = ScrollSmoother.create({
+// 	wrapper: ".smooth-wrapper",
+// 	content: ".smooth-content"
+// });

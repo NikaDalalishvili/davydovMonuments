703fa3e5237a8bebbcb611c89ae6710e418fcc2d
Dalala <nik.dalalishvili@gmail.com>
2023-03-09 21:08:36 +0100

    Ref V1.2.9-

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,13 @@
-var lastScrollTop = 0;
-var header = document.querySelector('header');
+// Get the header element
+let header = document.querySelector('header');
+let headerH = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-  var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (currentScrollTop > lastScrollTop) {
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function () {
+  let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScrollTop > lastScrollTop &&
+      window.pageYOffset >= headerH) {
     // scrolling down
     header.classList.add('header-hide');
   } else {

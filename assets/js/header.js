ffd4f274f154b5c6ffa620746d7a4564d3bdd73b
Dalala <nik.dalalishvili@gmail.com>
2023-03-12 17:07:34 +0100

    V1.3.3

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,5 @@
 // Get the header element
+let headerNav = document.querySelector('.header-nav');
 let headerB = document.querySelector('.header-btns');
 let headerH = headerB.offsetHeight;
 let mobileNav = document.querySelector('.mobile-nav');
@@ -12,9 +13,13 @@ window.addEventListener('scroll', function () {
       !mobileNav.classList.contains('is-active')) {
     // scrolling down
     headerB.classList.add('header-hide');
+    headerNav.classList.add('header-nav-hide');
+    console.log(headerH);
   } else {
     // scrolling up
+    console.log(window.pageYOffset + " PageY");
     headerB.classList.remove('header-hide');
+    headerNav.classList.remove('header-nav-hide');
   }
   lastScrollTop = currentScrollTop;
 });
